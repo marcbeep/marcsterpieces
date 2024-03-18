11f631741381ba73a8d34491eeb5c22f1791e7ee
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-03-18 09:26:56 +0000

    contract functions

--- myapp/contracts/Marcsterpiece.sol
+++ myapp/contracts/Marcsterpiece.sol
@@ -3,23 +3,46 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; // Import Ownable
 
-contract Marcsterpiece is ERC721URIStorage {
+contract Marcsterpiece is
+    ERC721URIStorage,
+    ReentrancyGuard,
+    Ownable // Inherit Ownable
+{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    mapping(string => uint8) private _uris;
+    uint256 public constant mintPrice = 0.01 ether;
+
     constructor() ERC721("Marcsterpiece", "MART") {}
+
+    function isUriOwned(string memory uri) public view returns (bool) {
+        return _uris[uri] == 1;
+    }
 
     function mintNFT(
         address recipient,
         string memory tokenURI
-    ) public returns (uint256) {
-        _tokenIds.increment();
+    ) public payable nonReentrant {
+        require(msg.value >= mintPrice, "Not enough Ether sent.");
+        require(!isUriOwned(tokenURI), "URI is already owned.");
 
+        _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
+        // Mark URI as owned
+        _uris[tokenURI] = 1;
+    }
+
+    // Withdraw function to transfer Ether from the contract to the owner
+    function withdraw() public onlyOwner {
+        // onlyOwner modifier now recognized
+        uint256 balance = address(this).balance;
+        payable(owner()).transfer(balance);
     }
 }
