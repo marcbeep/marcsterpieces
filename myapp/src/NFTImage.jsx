4d1a9a0e32e0e928c3966837b4c4312792f0bc4d
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-03-19 07:43:41 +0000

    NFTImage component

---
+++ myapp/src/NFTImage.jsx
@@ -0,0 +1,18 @@
+function NFTImage({ index, contentId }) {
+    const metadataUri = `${contentId}/${tokenId}.json`;
+    const imageUri = `https://gateway.pinata.cloud/ipfs/${contentId}/${tokenId}.png`;
+  
+    return (
+      <div className="card card-compact bg-base-100 shadow-xl">
+        <figure><img src={imageUri} alt={`NFT ${index}`} /></figure>
+        <div className="card-body">
+          <h2 className="card-title">NFT #{index}</h2>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Mint</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  export default NFTImage;
+  
